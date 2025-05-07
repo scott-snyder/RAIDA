05abc8d89d8f35622d9e685af887084622fc081d
sss <sss@karma>
2025-05-07 09:58:45 -0400

    Define AIDA_HAS_CAST to avoid warning in Gaudi.

--- include/AIDA/IBaseHistogram.h
+++ include/AIDA/IBaseHistogram.h
@@ -8,11 +8,13 @@
 #define AIDA_IBASEHISTOGRAM_H 1
 
 //  This file is part of the AIDA library
-//  Copyright (C) 2002 by the AIDA team.  All rights reserved.
+//  Copyright (C) 2002, 2025 by the AIDA team.  All rights reserved.
 //  This library is free software and under the terms of the
 //  GNU Library General Public License described in the LGPL.txt 
 
 #include <string>
+
+#define AIDA_HAS_CAST
 
 namespace AIDA {
 
